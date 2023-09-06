8c068bc9888e87d8a05576fc38ab365322e2fd70
manueledigregorio <dig.manuele@gmail.com>
2023-09-06 16:17:17 +0200

    [add] js

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const name = prompt("scrivi il tuo nome");
+const surname = prompt("scrivi il tuo cognome");
+const color = prompt("scrivi il tuo colore");
+const number = "23";
+
+const message = name + surname + color + number;
+
+document.getElementById("output").innerHTML = message;
+
+
